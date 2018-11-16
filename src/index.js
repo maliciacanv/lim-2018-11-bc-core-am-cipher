bcd0fdd10cc760bb801acae1a5ba17c7911be7e9
maliciacanv <cantenovictoriomaruja@gmail.com>
2018-11-16 16:51:04 -0500

    avance de botones de cifrar y descifrar

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// llamando al boton cifrar
+const botonCifrar= document.getElementById("boton1");
+// llamando al boton descifrar
+const botonDescifrar= document.getElementById("boton2");
+
+
+function cifrar(){
+}
+//evento del boton cifrar
+botonCifrar.addEventListener('click',cifrar);
