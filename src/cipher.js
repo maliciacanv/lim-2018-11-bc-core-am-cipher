d61afdbed1ea8dbafc0ea5df502ada37f9e1d516
maliciacanv <cantenovictoriomaruja@gmail.com>
2018-11-23 00:02:44 -0500

    funciones de eventos casi terminado

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,22 @@
 window.cipher = {
-  // ... 
+encode:(mensaje,desplazamiento) =>{
+  let palabraCifrada="";
+    for (let i=0 ; i<mensaje.length ; i++){
+  let nLetra=mensaje.charCodeAt(i);
+  const formulaC=(nLetra-65+desplazamiento)%26+65;
+  palabraCifrada=palabraCifrada+String.fromCharCode(formulaC);
+  }
+    return palabraCifrada;
+  },
+
+  decode:(mensaje,desplazamiento) => {
+  let palabraDescifrada="";
+    for (let i=0 ; i<mensaje.length ; i++){
+  let nuLetra=mensaje.charCodeAt(i);
+  const nuevFormula=(26-desplazamiento)%26;
+  const formulaD=(nuLetra+65+nuevFormula)%26+65;
+  palabraDescifrada=palabraDescifrada+String.fromCharCode(formulaD);
+    }
+    return palabraDescifrada;
+  }
 };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,53 @@
 // llamando al boton cifrar
-const botonCifrar= document.getElementById("boton1");
+const botonContinuar=document.getElementById("boton");
+//llamando al desplazamiento
+const clave=document.getElementById("desplazamiento");
+//llamando al mensaje
+const mensajeSecreto=document.getElementById("mensaje");
 // llamando al boton descifrar
-const botonDescifrar= document.getElementById("boton2");
+const botonCifrar=document.getElementById("boton2");
 
+const respuesta=document.getElementById("respuestas");
 
-function cifrar(){
+//llamando al boton para cifrar la palabra ingresada
+const botonDescifrar=document.getElementById("boton3");
+//llamando al boton seguirnavegando
+const botonNavegar=document.getElementById("boton4");
+
+//funcion continuar
+function continuar(){
+  document.getElementById("screen1").style="display:none";
+  document.getElementById("screen2").style="display:block";
 }
-//evento del boton cifrar
-botonCifrar.addEventListener('click',cifrar);
+//funciones cifrar
+function cifrar(){
+const clav=document.getElementById("desplazamiento").value;
+document.getElementById("claves").innerHTML=clav;
+respuesta.value=cipher.encode(mensajeSecreto.value,parseInt(clave.value));
+document.getElementById("screen2").style="display:none";
+document.getElementById("screen3").style="display:block";
+}
+
+//funcion descifrar
+function descifrar(){
+  document.getElementById("screen2").style="display:none";
+  document.getElementById("screen3").style="display:block";
+  const clav=document.getElementById("desplazamiento").value;
+  document.getElementById("claves").innerHTML=clav;
+  respuesta.value=cipher.decode(mensajeSecreto.value,parseInt(clave.value));
+}
+//funion navegar y que vuelva la inicio
+function regresar(){
+document.getElementById("screen3").style="display:none";
+document.getElementById("screen2").style="display:block";
+clave.value="";
+mensajeSecreto.value="";
+}
+//evento del boton continuar
+
+botonContinuar.addEventListener('click',continuar);
+//evento del boton cifrar y descifrar
+botonCifrar.addEventListener("click",cifrar);
+botonDescifrar.addEventListener("click",descifrar);
+//evento del boton navegar
+botonNavegar.addEventListener("click",regresar);
